4bdea571e99fecec812184b6d422c87861c1df40
dahyun <dahyun@naver.com>
2023-01-17 13:18:59 +0900

    Clicked Button, the message popped up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit
 
 from PyQt5.QtGui import QIcon
 
@@ -11,11 +11,14 @@ class Calcuator(QWidget):
         self.initUI()
 
     def initUI(self):
+        self.te1 = QPlainTextEdit()
+        self.te1.setReadOnly(True)
+
         self.btn1 = QPushButton('Message', self)
         self.btn1.clicked.connect(self.activateMessage)
 
         vbox = QVBoxLayout()
-        vbox.addStretch(1)
+        vbox.addWidget(self.te1)
         vbox.addWidget(self.btn1)
         vbox.addStretch(1)
 
@@ -27,8 +30,8 @@ class Calcuator(QWidget):
         self.show()
 
     def activateMessage(self):
-        QMessageBox.information(self, "information", "Button clicked!")
-
+        #QMessageBox.information(self, "information", "Button clicked!")
+        self.te1.appendPlainText("Button clicked!")
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
