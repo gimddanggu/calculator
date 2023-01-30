e7e3795755d56d139970c0b304e4be887b014160
dahyun <dahyun@naver.com>
2023-01-30 18:18:56 +0900

    Revert "Add in ui.py to print current date"
    This reverts commit a5aa48fe9c3e25ea6e169a7d3de059b064890462.

--- ui.py
+++ ui.py
@@ -1,20 +1,15 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, 
-                        QMessageBox, QPlainTextEdit, QHBoxLayout, QLabel)
-
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout
 
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QDate, Qt
 
 class View(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.date = QDate.currentDate()
         self.initUI()
-
+        
     def initUI(self):
-        self.lbl1 = QLabel(self.date.toString(Qt.DefaultLocaleLongDate), self)
         self.te1 = QPlainTextEdit()
         self.te1 .setReadOnly(True)
 
@@ -29,8 +24,7 @@ class View(QWidget):
         vbox = QVBoxLayout()
         vbox.addWidget(self.te1)
         vbox.addLayout(hbox)
-        vbox.addWidget(self.lbl1)
-        #vbox.addStretch(1)
+        vbox.addStretch(1)
         
 
         self.setLayout(vbox)
